278dcf88f5eba9d9c6a33bcd875854db13adc5a6
Juan David Julio Serrano <88696888+juanDjulioS@users.noreply.github.com>
2023-05-21 17:29:00 -0500

    Update color_function.py
    cambié azul por amarillo

--- color_function.py
+++ color_function.py
@@ -3,8 +3,8 @@ def color_detector(frame):
     # Definir los rangos de colores en HSV
     red_lower = np.array([0, 120, 70])
     red_upper = np.array([10, 255, 255])
-    blue_lower = np.array([100, 150, 0])
-    blue_upper = np.array([140, 255, 255])
+    yellow_lower = np.array([20, 100, 100])
+    yellow_upper = np.array([30, 255, 255])
     green_lower = np.array([36, 25, 25])
     green_upper = np.array([70, 255,255])
 
@@ -13,33 +13,33 @@ def color_detector(frame):
 
     # Crear máscaras para cada color
     mask_red = cv2.inRange(hsv, red_lower, red_upper)
-    mask_blue = cv2.inRange(hsv, blue_lower, blue_upper)
+    mask_yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
     mask_green = cv2.inRange(hsv, green_lower, green_upper)
 
     # Aplicar una operación de apertura a las máscaras para eliminar pequeños grupos de píxeles falsos positivos
     kernel = np.ones((5,5),np.uint8)
     mask_red = cv2.morphologyEx(mask_red, cv2.MORPH_OPEN, kernel)
-    mask_blue = cv2.morphologyEx(mask_blue, cv2.MORPH_OPEN, kernel)
+    mask_yellow = cv2.morphologyEx(mask_yellow, cv2.MORPH_OPEN, kernel)
     mask_green = cv2.morphologyEx(mask_green, cv2.MORPH_OPEN, kernel)
 
     # Contar el número de píxeles de cada color
     red_count = cv2.countNonZero(mask_red)
-    blue_count = cv2.countNonZero(mask_blue)
+    yellow_count = cv2.countNonZero(mask_yellow)
     green_count = cv2.countNonZero(mask_green)
 
     # Determinar el color dominante
-    if red_count > blue_count and red_count > green_count:
+    if red_count > yellow_count and red_count > green_count:
         color = "red"
         color_code = (0, 0, 255)
-    elif blue_count > red_count and blue_count > green_count:
-        color = "blue"
-        color_code = (255, 0, 0)
+    elif yellow_count > red_count and yellow_count > green_count:
+        color = "yellow"
+        color_code = (0, 255, 255)
     else:
         color = "green"
         color_code = (0, 255, 0)
 
     # Crear una máscara para todos los colores
-    mask = cv2.bitwise_or(mask_red, mask_blue)
+    mask = cv2.bitwise_or(mask_red, mask_yellow)
     mask = cv2.bitwise_or(mask, mask_green)
 
     # Aplicar una operación de apertura a la máscara para eliminar pequeños grupos de píxeles blancos
