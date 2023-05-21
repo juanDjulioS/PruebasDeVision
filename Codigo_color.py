9871214367a21b01cb60d869c45dc22522c4592d
Juan David Julio Serrano <88696888+juanDjulioS@users.noreply.github.com>
2023-05-20 23:19:24 -0500

    Update Codigo_color.py
    arreglar recuadro delimitador de color dominante

--- Codigo_color.py
+++ Codigo_color.py
@@ -27,6 +27,12 @@ while True:
     mask_blue = cv2.inRange(hsv, blue_lower, blue_upper)
     mask_green = cv2.inRange(hsv, green_lower, green_upper)
 
+    # Aplicar una operación de apertura a las máscaras para eliminar pequeños grupos de píxeles falsos positivos
+    kernel = np.ones((5,5),np.uint8)
+    mask_red = cv2.morphologyEx(mask_red, cv2.MORPH_OPEN, kernel)
+    mask_blue = cv2.morphologyEx(mask_blue, cv2.MORPH_OPEN, kernel)
+    mask_green = cv2.morphologyEx(mask_green, cv2.MORPH_OPEN, kernel)
+
     # Contar el número de píxeles de cada color
     red_count = cv2.countNonZero(mask_red)
     blue_count = cv2.countNonZero(mask_blue)
@@ -43,13 +49,25 @@ while True:
         color = "Verde"
         color_code = (0, 255, 0)
 
-        # Crear una máscara para todos los colores
-    mask = mask_red + mask_blue + mask_green
+    # Crear una máscara para todos los colores
+    mask = cv2.bitwise_or(mask_red, mask_blue)
+    mask = cv2.bitwise_or(mask, mask_green)
 
-    # Dibujar un recuadro alrededor de la figura utilizando operaciones vectorizadas
-    y,x = np.where(mask > 0)
-    x1,y1,x2,y2 = np.min(x), np.min(y), np.max(x), np.max(y)
-    cv2.rectangle(frame,(x1,y1),(x2,y2),color_code,2)
+    # Aplicar una operación de apertura a la máscara para eliminar pequeños grupos de píxeles blancos
+    kernel = np.ones((5,5),np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+    # Encontrar los contornos en la máscara
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Definir el valor umbral para el área del contorno
+    color_area_threshold = 250
+
+    # Dibujar un recuadro alrededor de cada contorno cuya área sea mayor que el umbral
+    for c in contours:
+        if cv2.contourArea(c) > color_area_threshold:
+            x,y,w,h = cv2.boundingRect(c)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),color_code,2)
 
     # Mostrar el nombre del color en la esquina superior derecha
     cv2.putText(frame,color,(frame.shape[1]-100,50),cv2.FONT_HERSHEY_SIMPLEX,1,color_code,2,cv2.LINE_AA)
