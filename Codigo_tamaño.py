1752632cca637f87522adb85d9987b850055fa9c
Juan David Julio Serrano <88696888+juanDjulioS@users.noreply.github.com>
2023-05-21 01:06:06 -0500

    Create Codigo_tamaño.py
    TIene la funcion detect_shape(c) incorporada y la funcion main(option) y su llamada

---
+++ Codigo_tamaño.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+
+def detect_shape(c):
+    # Inicializar el nombre de la forma y el perímetro aproximado
+    shape = "unidentified"
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+    # Si el contorno tiene 3 vértices, entonces es un triángulo
+    if len(approx) == 3:
+        shape = "Triangle"
+
+    # Si el contorno tiene 4 vértices, entonces es un cuadrado o un rectángulo
+    elif len(approx) == 4:
+        # Calcular el bounding box del contorno y usar el bounding box para calcular la relación de aspecto
+        (x, y, w, h) = cv2.boundingRect(approx)
+        ar = w / float(h)
+
+        # Un cuadrado tendrá una relación de aspecto aproximadamente igual a uno, de lo contrario, el contorno es un rectángulo
+        shape = "Square" if ar >= 0.95 and ar <= 1.05 else "Rectangle"
+
+    # Si el contorno es un círculo
+    else:
+        shape = "Circle"
+
+    # Devolver el nombre de la forma
+    return shape
+    
+def size_detector(pixels_per_cm):
+    # Cargar la imagen desde la cámara web
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        # Capturar un nuevo frame
+        ret, frame = cap.read()
+
+        # Convertir la imagen a escala de grises y aplicar un desenfoque para suavizarla
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+        # Realizar una detección de bordes y encontrar los contornos en la imagen
+        edged = cv2.Canny(blurred, 50, 100)
+        contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        
+        # Definir el valor umbral para el área del contorno
+        shape_area_threshold = 150
+        
+        # Recorrer cada contorno
+        for c in contours:
+             # Verificar si el contorno es cerrado
+            if cv2.contourArea(c) > shape_area_threshold:
+                # Calcular el centro del contorno y dibujar un círculo en el centro
+                M = cv2.moments(c)
+                if M["m00"] != 0:
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    shape = detect_shape(c)
+                    cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+                    (x, y, w, h) = cv2.boundingRect(c)
+                    if shape =="Circle":
+                        radius = w/2
+                        diameter = w / pixels_per_cm
+                        size_text = "Diameter: {:.1f}cm".format(diameter)
+                        cv2.putText(frame,size_text,(cX - 20 , cY - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255, 255, 255), 2)
+                    elif shape == "Triangle":
+                        side = w / pixels_per_cm
+                        size_text = "Side: {:.1f}cm".format(side)
+                        cv2.putText(frame, size_text, (cX - 20 , cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                    else:
+                        width = w / pixels_per_cm
+                        height = h / pixels_per_cm
+                        size_text = "{:.1f}cm x {:.1f}cm".format(width, height)
+                        cv2.putText(frame,size_text,(cX - 20 , cY - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255, 255, 255), 2)
+
+        # Mostrar la imagen final
+        cv2.imshow('frame',frame)
+
+        # Verificar si el usuario presionó la tecla 'q' para salir
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def main(option):
+    if option == 0:
+        #color_detector()
+        pass
+    elif option == 1:
+        pass
+        #shape_detector()
+    elif option == 2:
+        pass
+        size_detector(pixels_per_cm=50)
+
+if __name__ == '__main__':
+    main(2)
