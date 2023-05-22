3fbe880a5d9041b8e9393796d0a91403794f51d1
Juan David Julio Serrano <88696888+juanDjulioS@users.noreply.github.com>
2023-05-22 10:16:02 -0500

    Update detect_shape_function.py
    se aumentaron las restricciones geometricas para las figuras, para que disminuyeran las lectura erroneas

--- detect_shape_function.py
+++ detect_shape_function.py
@@ -1,21 +1,40 @@
 # Función para detectar la forma, se usa tanto en forma como tamaño
 def detect_shape(c):
+    
+    servo_code = None
+    
     # Inicializar el nombre de la forma y el perímetro aproximado
-    shape = "unidentified"
+    shape = None
     peri = cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, 0.04 * peri, True)
 
     # Si el contorno tiene 3 vértices, entonces es un triángulo
     if len(approx) == 3:
-        shape = "Triangle"
+        # Calcular la distancia entre dos puntos del contorno
+        side = np.sqrt((approx[0][0][0] - approx[1][0][0]) ** 2 + (approx[0][0][1] - approx[1][0][1]) ** 2)
+       
+        # Calcular la altura esperada de un triángulo equilátero
+        expected_height = np.sqrt(3) / 2 * side
+        
+        # Obtener la altura real del contorno
+        (_, _, _, h) = cv2.boundingRect(c)
+        
+        # Verificar si la altura real es similar a la altura esperada
+        if abs(expected_height - h) <= 0.1 * expected_height:  # Ajusta este valor según sea necesario
+            shape = "Triangle"
+            servo_code = '0'
 
     # Si el contorno tiene 4 vértices, entonces es un cuadrado o un rectángulo
     elif len(approx) == 4:
-        shape = "Square" 
+        shape = "Square"
+        servo_code = '1'
 
     # Si el contorno es un círculo
     else:
-        shape = "Circle"
+        (_, _, w, h) = cv2.boundingRect(c)
+        if abs(w - h) <= 0.1 * w:  # Ajusta este valor según sea necesario
+            shape = "Circle"
+            servo_code = '2'
 
     # Devolver el nombre de la forma
-    return shape
+    return servo_code, shape
