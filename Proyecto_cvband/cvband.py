43b1ea81a55177d14de83a21813d65ab3821f2e3
Juan David Julio Serrano <88696888+juanDjulioS@users.noreply.github.com>
2023-05-28 21:26:06 -0500

    Update cvband.py

--- Proyecto_cvband/cvband.py
+++ Proyecto_cvband/cvband.py
@@ -3,7 +3,7 @@ import numpy as np
 import serial.tools.list_ports
 
 MAX_SAMPLES = 100  # número máximo de muestras a almacenar
-WEBCAM = 1
+WEBCAM = 0
 LARGE_SIZE_THRESHOLD = 4.0
 MEDIUM_SIZE_THRESHOLD = 3.0
 CIRCLE_DIAMETER = 4.0
@@ -74,7 +74,7 @@ def color_detector(frame,draw=True):
             cv2.rectangle(frame,(x,y),(x+w,y+h),color_code,2)
         cv2.putText(frame,color,(frame.shape[1]-100,50),cv2.FONT_HERSHEY_SIMPLEX,1,color_code,2,cv2.LINE_AA)
 
-    return servo_code, frame
+    return servo_code, frame, mask
 
 # Función para detectar la forma, se usa tanto en forma como tamaño
 def detect_shape(c):
@@ -114,40 +114,73 @@ def detect_shape(c):
     # Devolver el nombre de la forma
     return servo_code, shape
 
-def shape_detector(frame):
+def shape_detector(frame, mask):
+        # Verificar que la máscara no esté vacía
+    if mask is None or mask.size == 0:
+        return None, None
     
-    servo_code = None
+    # Suavizar la máscara
+    mask = cv2.medianBlur(mask, 7)
     
-    # Convertir la imagen a escala de grises
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Encontrar los contornos en la máscara
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
-     # Mejorar el contraste de la imagen en escala de grises
-    gray = cv2.equalizeHist(gray)
- 
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    # Ordenar los contornos por área y tomar el más grande
+    contour = sorted(contours, key=cv2.contourArea, reverse=True)[0]
+    
+    # Aproximar el contorno a una forma geométrica
+    peri = cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, 0.04 * peri, True)
+        
+    # Inicializar las variables shape y code
+    shape = None
+    code = None
+    
+    # Asignar valores a shape y code según la forma geométrica detectada
+    if len(approx) == 3:
+        shape = "Triangle"
+        code = '0'
+    elif len(approx) == 4:
+            shape = "Square"
+            code = '1'
+    elif len(approx) > 4:
+        # Calcular la relación entre el área del contorno y el área del círculo que lo circunscribe
+        area = cv2.contourArea(contour)
+        (x, y), radius = cv2.minEnclosingCircle(contour)
+        circle_area = np.pi * (radius ** 2)
+        circle_ratio = area / circle_area
+        
+        # Verificar si la relación entre el área del contorno y el área del círculo es cercana a 1
+        if 0.7 <= circle_ratio <= 1.3:
+            shape = "Circle"
+            code = '2'
+    # Dibujar formas geométricas correspondientes
+    if shape == "Triangle":
+        # Dibujar triángulo
+        cv2.drawContours(frame, [approx], -1, (0, 255, 0), 2)
+    elif shape == "Square":
+        # Obtener el rectángulo delimitador mínimo que ajusta al contorno
+        rotated_rect = cv2.minAreaRect(contour)
+        box = cv2.boxPoints(rotated_rect)
+        box = np.int0(box)
+            
+        # Dibujar rectángulo rotado
+        cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
+    elif shape == "Circle":
+        # Obtener el centro y el radio del círculo
+        (x, y), radius = cv2.minEnclosingCircle(approx)
+        center = (int(x), int(y))
+        radius = int(radius)
+        # Dibujar círculo
+        cv2.circle(frame, center, radius, (0, 255, 0), 2)
+    M = cv2.moments(contour)
+    if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255, 255, 255), 2)
 
-    # Realizar una detección de bordes y encontrar los contornos en la imagen
-    edged = cv2.Canny(blurred, 50, 100)
-    contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    
-    # Definir el valor umbral para el área del contorno
-    shape_area_threshold = 250
-    
-    # Recorrer cada contorno
-    for c in contours:
-            # Verificar si el contorno es cerrado
-        if cv2.contourArea(c) > shape_area_threshold:
-            # Calcular el centro del contorno y dibujar un círculo en el centro
-            M = cv2.moments(c)
-            if M["m00"] != 0:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                servo_code, shape = detect_shape(c)
-                cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
-                cv2.putText(frame, shape, (cX - 20 , cY - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255, 255, 255), 2)
+    return code, frame
 
-    # Devuelve la imagen final
-    return servo_code, frame
 
 def size_detector(frame, pixels_per_cm, large_size_threshold, medium_size_threshold):
     
